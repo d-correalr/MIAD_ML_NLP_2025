167db18b9a66cd1abdb5cfb963a82ab2199f2c76
d-correalr <d.correalr@uniandes.edu.co>
2025-04-26 07:25:50 -0500

    update m_model

--- model_deployment/m_model_deployment.py
+++ model_deployment/m_model_deployment.py
@@ -1,15 +1,31 @@
+import pandas as pd
+import joblib
+import sys
+import os
 
 def predict_popularity(duration, danceability, valence):
-    import pandas as pd
-    import joblib
-    import os
+    model = joblib.load(os.path.dirname(__file__) + '/modelo_musica.pkl')
     
-    model = joblib.load(os.path.join(os.path.dirname(__file__), 'modelo_musica.pkl'))
-
     data = pd.DataFrame([{
         'duration_ms': duration,
         'danceability': danceability,
         'valence': valence
     }])
+    
+    pred = model.predict(data)[0]
+    return pred
 
-    return model.predict(data)[0]
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print('Por favor ingresa: duration_ms, danceability, y valence')
+    else:
+        duration_ms = float(sys.argv[1])
+        danceability = float(sys.argv[2])
+        valence = float(sys.argv[3])
+        
+        result = predict_popularity(duration_ms, danceability, valence)
+        
+        print(f"Duración: {duration_ms} ms")
+        print(f"Danceabilidad: {danceability}")
+        print(f"Valencia: {valence}")
+        print(f"Popularidad predicha (clase): {result}")
