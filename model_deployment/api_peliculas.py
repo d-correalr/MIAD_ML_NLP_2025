012584ad13f5b0e78b58d61de4894d9743fdb57d
d-correalr <d.correalr@uniandes.edu.co>
2025-05-24 10:24:50 -0500

    Api Peliculas

---
+++ model_deployment/api_peliculas.py
@@ -0,0 +1,48 @@
+from flask import Flask 
+from flask import Flask
+from flask_restx import Api, Resource, reqparse
+from m_model_deployment_peliculas import predict_genre
+
+app = Flask(__name__)
+
+api = Api(
+    app,
+    version='1.0',
+    title='Predicción Género de una Pelicula',
+    description='API que predice el género de una película con base en algunas caracteristicas',
+    mask=None  
+)
+
+ns = api.namespace('predict', description='Predicción de género de películas')
+
+parser = reqparse.RequestParser(trim=True)
+
+parser.add_argument('year', type=float, required=True,
+                    help='Año de la película (1984 a 2015)', location='args')
+parser.add_argument('rating', type=float, required=True,
+                    help='rating de la película (1.2 a 9.3)', location='args')
+parser.add_argument('title', type=str, required=True,
+                    help='Titulo de la pelicula', location='args')
+parser.add_argument('plot', type=str, required=True,
+                    help='Trama de la pelicula', location='args')
+
+@ns.route('/')
+class GenreApi(Resource):
+    @api.expect(parser)
+    def get(self):
+        args = parser.parse_args()
+        try:
+            result = predict_genre(**args)
+            return {
+    "input": args,
+    "prediction": {
+        "genre": result["predicted_genre"],
+        "popularity": result["popularity_category"]
+    }
+}, 200
+
+        except ValueError as e:
+            return {'message': str(e)}, 400
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
